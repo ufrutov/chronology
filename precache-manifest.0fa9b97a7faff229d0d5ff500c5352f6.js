5920be54f9657a3a173e9173a7b33392606d02e3
ufrutov <mailluifruti@mail.ru>
2020-01-29 16:41:24 +0200

    Updates

---
+++ precache-manifest.0fa9b97a7faff229d0d5ff500c5352f6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b73590cdb68f939299d62949fcc8bca",
+    "url": "/index.html"
+  },
+  {
+    "revision": "299b215799ba654a9274",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "1ad86ad2cd342c013602",
+    "url": "/static/css/main.3a31f4a6.chunk.css"
+  },
+  {
+    "revision": "299b215799ba654a9274",
+    "url": "/static/js/2.ebb90506.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.ebb90506.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1ad86ad2cd342c013602",
+    "url": "/static/js/main.d38edda0.chunk.js"
+  },
+  {
+    "revision": "14fd8f94cef484f6b138",
+    "url": "/static/js/runtime-main.78f436ef.js"
+  }
+]);
